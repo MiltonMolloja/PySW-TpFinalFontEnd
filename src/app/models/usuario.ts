84aca2a365b6efe05aa5edad77732df49b19358c
oscar ugarte <oscar.ugrt@gmail.com>
2019-06-19 18:37:53 -0300

    Cargado los campos de perfil y usuario.

--- src/app/models/usuario.ts
+++ src/app/models/usuario.ts
@@ -1,2 +1,29 @@
-export class Usuario {
+import { Perfil } from './perfil';
+import { Escribano } from './escribano';
+
+
+export class Usuario
+{
+    id:number;
+    username:string;
+    password:string;
+    email:string;
+    tipo:string;
+    imagen:string;
+    estado:boolean;
+    perfil:Perfil;
+    escribano:Escribano;
+
+    constructor(id?:number, username?:string, password?:string, email?:string, tipo?:string, imagen?:string, estado?:boolean, perfil?:Perfil, escribano?:Escribano)
+    {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.tipo = tipo;
+        this.imagen = imagen;
+        this.estado = estado;
+        this.perfil = perfil;
+        this.escribano = escribano;
+    }
 }
