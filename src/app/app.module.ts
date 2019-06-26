3716b243ec79f6f86266867a2ae701bd3762dfb7
Milton Fernado Molloja <milton.molloja@gmail.com>
2019-06-26 03:52:01 -0300

    Delet Test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,11 +17,6 @@ import { LoginService } from './services/login.service';
 //Cuando se quiere trabajar con ngModel
 import { FormsModule } from '@angular/forms';
 import { AdministradorComponent } from './components/administrador/administrador.component';
-import { EscribanoTestComponent } from './components/escribano-test/escribano-test.component';
-import { PerfilTestComponent } from './components/perfil-test/perfil-test.component';
-import { GestionEscribaniasComponent } from './components/gestion-escribania/gestion-escribania.component';
-import { GestionNovedadComponent } from './components/gestion-novedad/gestion-novedad.component';
-import { NavbarTipoTestComponent } from './components/navbar-tipo-test/navbar-tipo-test.component';
 import { UsuarioComponent } from './components/usuario/usuario.component';
 import { PerfilComponent } from './components/perfil/perfil.component';
 import { EscribanoComponent } from './components/escribano/escribano.component';
@@ -38,17 +33,12 @@ import { NavigationBarComponent } from './components/navigation-bar/navigation-b
     HomeComponent,
     LoginComponent,
     AdministradorComponent,
-    EscribanoTestComponent,
-    PerfilTestComponent,
-    NavbarTipoTestComponent,
     GerenteComponent,
     NovedadComponent,
     EscribaniaComponent,
     EscribanoComponent,
     UsuarioComponent,
     PerfilComponent,
-    GestionNovedadComponent,
-    GestionEscribaniasComponent,
     NavigationBarComponent
 
   ],
